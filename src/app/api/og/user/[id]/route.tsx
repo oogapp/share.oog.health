ccda4a539ebe187e8980d637fed29612e8cc8ebb
Sean Clarke <456713+seanclarke@users.noreply.github.com>
2024-11-20 09:49:58 -0600

    add profile photo

--- src/app/api/og/user/[id]/route.tsx
+++ src/app/api/og/user/[id]/route.tsx
@@ -7,6 +7,7 @@ export const runtime = 'edge';
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
 
     let user = await getUser(params.id);
+    let profileImageUrl = user.profileImage?.url;
 
     return new ImageResponse(
         (
@@ -19,13 +20,26 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
                     background: 'black',
                     width: '100%',
                     height: '100%',
-                    padding: '50px 50px',
+                    padding: '25px 25px',
                     textAlign: 'left',
                     position: 'relative',
                 }}>
-                <div style={{ display: 'flex', fontSize: 64, color: "#A4F421" }}>
-                    {user.firstName} {user.lastName}
+                <div style={{ display: 'flex', flexDirection: 'column', fontSize: 64, color: "#A4F421" }}>
+                    <div style={{ display: 'flex' }}>{user.firstName} {user.lastName} {user.credential}</div>
+                    <div style={{ display: 'flex', fontSize: 32 }}>
+                        {user?.npiTaxonomyDescription}
+                    </div>
                 </div>
+
+                {profileImageUrl && (
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <img
+                            style={{ width: 350, height: 350, borderRadius: 175, marginTop: 20 }}
+                            src={profileImageUrl + "preview"}
+                            alt="profile"
+                        />
+                    </div>
+                )}
 
                 <div style={{ display: 'flex', position: 'absolute', right: 20, bottom: 20 }}>
                     <img style={{ width: 150 }} src="https://learn.oog.health/logo.svg" alt="oog" />
