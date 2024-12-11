7666c9658ea9ce757deffc3a228d76af2258ca14
Sean Clarke <456713+seanclarke@users.noreply.github.com>
2024-12-11 10:19:40 -0600

    screen height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,14 +32,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full dark" style={{ colorScheme: 'dark', backgroundColor: 'black' }} suppressHydrationWarning>
+    <html lang="en" className="h-screen dark" style={{ colorScheme: 'dark', backgroundColor: 'black' }} suppressHydrationWarning>
       <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no' />
       <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
       <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
       <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
       <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
       <body style={{ backgroundColor: 'black' }} className={`h-full ${marsden.variable}`}>
-        <div className="max-w-lg mx-auto relative h-full">{children}</div>
+        <div className="max-w-lg mx-auto relative h-screen">{children}</div>
       </body>
     </html>
   );
