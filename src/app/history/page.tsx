1f7b02bbabf6b45d09d35719481372542d9cb8ff
Sean Clarke <456713+seanclarke@users.noreply.github.com>
2024-12-16 12:39:43 -0600

    add back button

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,8 +1,11 @@
 import SearchHistory from "@/components/SearchHistory";
+import { ChevronLeft } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
+
 
 export const metadata: Metadata = {
-    title: "Previous Searches",
+    title: "Previous",
 };
 
 
@@ -10,6 +13,11 @@ export default function SearchHistoryPage() {
 
     return (
         <div>
+            <div className="p-3 flex items-center">
+                <Link href="/chat">
+                    <ChevronLeft className="h-6 w-6" />
+                </Link>
+            </div>
             <div className="my-">
                 <SearchHistory />
             </div>
