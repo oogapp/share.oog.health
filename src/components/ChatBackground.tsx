f8b5cb6cb725abd0fd7d562e94bb2f6aebc8432a
Sean Clarke <456713+seanclarke@users.noreply.github.com>
2025-01-06 13:30:44 -0600

    draw out the fade

--- src/components/ChatBackground.tsx
+++ src/components/ChatBackground.tsx
@@ -6,7 +6,7 @@ export const ChatBackground = () => (
         className="z-0 relative"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 1 }}
     >
         <svg
             className='w-full h-3/4'
