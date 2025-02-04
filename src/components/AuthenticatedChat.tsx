1915a7b9a8eee24890bf0eb10e9fd1b71f9fdce7
Sean Clarke <456713+seanclarke@users.noreply.github.com>
2025-02-04 09:35:54 -0600

    handlers to support native congrats

--- src/components/AuthenticatedChat.tsx
+++ src/components/AuthenticatedChat.tsx
@@ -15,7 +15,7 @@ import { MessageVariant } from '@/lib/utils';
 import { ClockIcon } from 'lucide-react';
 import { motion } from "motion/react";
 import Link from 'next/link';
-import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import {
     Channel,
     Chat,
@@ -25,9 +25,7 @@ import {
     Window,
     defaultRenderMessages,
     useChannelStateContext,
-    useChatContext,
-    useCreateChatClient,
-    useMessageListContext
+    useCreateChatClient
 } from 'stream-chat-react';
 import 'stream-chat-react/dist/css/v2/index.css';
 import Citation from './Citation';
@@ -47,45 +45,6 @@ const customRenderText = (text: string) => {
     text = text.replaceAll(")]", ")")
     return text
 };
-
-function SparkyThinking() {
-    const { client } = useChatContext();
-    const [show, setShow] = useState(false)
-    const { listElement, scrollToBottom } = useMessageListContext();
-    const [thinkingLabel, setThinkingLabel] = useState('Analyzing query...')
-
-    useLayoutEffect(() => {
-        scrollToBottom()
-    }, [show])
-
-    useEffect(() => {
-        const handleEvent = (event: any) => {
-            if (event.type == "sparky_thinking_start") {
-                if (event.label) {
-                    setThinkingLabel(event.label)
-                }
-                setShow(true)
-            }
-            if (event.type == "sparky_thinking_stop") {
-                setShow(false)
-            }
-        }
-        client.on(handleEvent);
-        return () => client.off(handleEvent);
-    }, [client]);
-
-    if (!show) return null;
-
-    return (
-        <div className='flex items-center gap-x-2 my-6 px-3'>
-            <svg className="animate-spin h-5 w-5 text-brand" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-            <div className='text-sm text-white'>{thinkingLabel}</div>
-        </div>
-    )
-}
 
 function EmptyStateIndicator() {
 
@@ -154,7 +113,6 @@ function EmptyStateIndicator() {
 
 const customRenderMessages: MessageRenderer<DefaultStreamChatGenerics> = (options) => {
     const elements = defaultRenderMessages(options);
-    //elements.push(<SparkyThinking key={'sparky_thinking'} />);
     return elements;
 };
 
@@ -226,6 +184,33 @@ export default function AuthenticatedChat({ userId, token, channelId, apiKey, me
 
     const [channel, setChannel] = useState<any | null>(null);
 
+    async function handleCongrats() {
+        trackAnalytics("Credit - Earned", {
+            userId: currentUser.id,
+            reason: "medicalSearchReflection",
+            conversationId: chat?.id!,
+            creditAmount: 0.5,
+        })
+
+        let wk = (window as any).webkit
+        if (wk?.messageHandlers?.earnCe) {
+            try {
+                let payload = {
+                    type: "earnCe",
+                    payload: { type: "earnCe", payload: { credits: 0.5 } }
+                }
+                wk.messageHandlers.earnCe.postMessage(payload)
+                return;
+            } catch (e) {
+                console.error(`Error invoking earnCe: ${e}`)
+                setShowAnimation(true)
+            }
+        } else {
+            setShowAnimation(true)
+        }
+
+    }
+
     async function handleSetChannel() {
         if (client) {
             let channels = await client.queryChannels({ type: 'reflection_v1', id: { $in: [channelId] } });
@@ -235,13 +220,7 @@ export default function AuthenticatedChat({ userId, token, channelId, apiKey, me
                 chan.on(event => {
                     //@ts-ignore
                     if (event.type == "ce_credit_earned") {
-                        setShowAnimation(true)
-                        trackAnalytics("Credit - Earned", {
-                            userId: currentUser.id,
-                            reason: "medicalSearchReflection",
-                            conversationId: chat?.id!,
-                            creditAmount: 0.5,
-                        })
+                        handleCongrats()
                     }
                     if (event.type == "notification.mark_read") {
                         client.markAllRead()
--- src/components/CurrentUserContext.tsx
+++ src/components/CurrentUserContext.tsx
@@ -1,13 +1,28 @@
 'use client'
 import { User } from "@/gql/graphql";
 // create a video context to switch the video background based on if the user is currentlying searching
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 export const CurrentUserContext = createContext({
     user: {} as User,
 });
 
 export const CurrentUserContextProvider = ({ user, children }: { user: User, children: React.ReactNode }) => {
+
+    useEffect(() => {
+        const handleMessage = (event: MessageEvent) => {
+            if (event.data.type === "askAnotherQuestion") {
+                window.location.href = "/chat";
+            }
+        };
+
+        window.addEventListener("message", handleMessage);
+
+        return () => {
+            window.removeEventListener("message", handleMessage);
+        };
+    }, []);
+
     return (
         <CurrentUserContext.Provider value={{ user }}>
             {children}
